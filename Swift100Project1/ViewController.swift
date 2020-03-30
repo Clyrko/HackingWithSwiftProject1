4b152d8623576d56159e4fba932e08b186d83bb3
Jay A <justynadusei@gmail.com>
2020-03-30 11:20:00 +0000

    Initial Commit

---
+++ Swift100Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift100Project1
+//
+//  Created by Jay A. on 3/30/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
